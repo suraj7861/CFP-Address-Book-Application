ccbe2ab9bf6d680a03383f40c5dbf397b9a3e074
unknown <khomanesuraj8@gmail.com>
2021-12-24 10:33:22 +0530

    Created Address book Application Spring Project and print welcome message

--- src/main/java/com/bridgelabz/addressbookapp/AddressBookApplication.java
+++ src/main/java/com/bridgelabz/addressbookapp/AddressBookApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AddressBookApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Apllication");
 		SpringApplication.run(AddressBookApplication.class, args);
 	}
 
